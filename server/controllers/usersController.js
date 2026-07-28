c931b359623b5a930968806b085fee840a218632
gmapunda <mapundagrayson@gmail.com>
2026-07-28 18:36:30 +0300

    Login Functionality

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,4 @@
+const { decrypt } = require("dotenv");
 const pool = require("../db");
 const bcrypt = require("bcrypt");
 
@@ -5,7 +6,6 @@ const getAllUsers = async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM users");
 
-    console.log("USERS: ", result.rows);
     res.status(200).json({
       message: "Register user works",
       users: result.rows,
@@ -54,7 +54,6 @@ const registerUser = async (req, res) => {
       [name, email, hashedPassword]
     );
 
-    console.log("RESULTS: ", result.rows[0]);
     const user = result.rows[0];
     res.status(201).json({
       user: {
@@ -64,11 +63,57 @@ const registerUser = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({
       message: "Server error",
     });
   }
 };
 
-module.exports = { registerUser, getAllUsers };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const result = await pool.query("SELECT * FROM users WHERE email=$1", [
+      email,
+    ]);
+
+    const user = result.rows[0];
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    //compare for password
+    const loginTest = await bcrypt.compare(password, user.password);
+
+    if (loginTest) {
+      res.status(200).json({
+        message: "User loged in successfully",
+        user: {
+          name: user.name,
+          email: user.email,
+        },
+      });
+    } else {
+      return res.status(400).json({
+        message: "Email or password is incorect",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = { registerUser, getAllUsers, login };
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerUser, getAllUsers } = require("../controllers/usersController");
+const {
+  registerUser,
+  getAllUsers,
+  login,
+} = require("../controllers/usersController");
 
 router.get("/", getAllUsers);
 router.post("/", registerUser);
+router.post("/login", login);
 
 module.exports = router;
