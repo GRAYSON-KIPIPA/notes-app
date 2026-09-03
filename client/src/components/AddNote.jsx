7631a70e252efe6a3f54e5fea6c0bf5f42d00fbb
gmapunda <mapundagrayson@gmail.com>
2026-09-03 09:58:30 +0300

    Add Validation in client side while adding a Note

--- client/src/components/AddNote.jsx
+++ client/src/components/AddNote.jsx
@@ -9,25 +9,81 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { addNote } from "../services/notesService";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useNavigate } from "react-router";
 
 function AddNote() {
   const [categories, setCategories] = React.useState([]);
-  const [category_id, setCategory_id] = React.useState();
+  const [category_id, setCategory_id] = React.useState("");
   const [title, setTitle] = React.useState("");
   const [content, setContent] = React.useState("");
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState("");
+  const [errors, setErrors] = React.useState({
+    title: "",
+    content: "",
+    category_id: "",
+  });
+  const navigate = useNavigate();
 
+  const validateForm = () => {
+    const newErrors = {
+      title: "",
+      content: "",
+      category_id: "",
+    };
+
+    let isValid = true;
+
+    if (!title.trim()) {
+      newErrors.title = "Title is required";
+      isValid = false;
+    } else if (title.trim().length < 2) {
+      newErrors.title = "Title must have at least 2 characters";
+      isValid = false;
+    }
+
+    if (!content.trim()) {
+      newErrors.content = "Content is required";
+      isValid = false;
+    } else if (content.trim().length < 2) {
+      newErrors.content = "Content must have at least 2 characters";
+      isValid = false;
+    }
+
+    if (!category_id) {
+      newErrors.category_id = "Please select a category";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+
+    return isValid;
+  };
   const handleChangeCategoryId = (event) => {
-    setCategory_id(event.target.value);
+    setCategory_id(Number(event.target.value));
+
+    setErrors((prev) => ({
+      ...prev,
+      category_id: "",
+    }));
   };
 
   const handleChangeTitle = (event) => {
     setTitle(event.target.value);
+
+    setErrors((prev) => ({
+      ...prev,
+      title: "",
+    }));
   };
 
   const handleChangeContent = (event) => {
     setContent(event.target.value);
+
+    setErrors((prev) => ({
+      ...prev,
+      content: "",
+    }));
   };
 
   const handleGetAllCategories = async () => {
@@ -36,17 +92,42 @@ function AddNote() {
   };
 
   const handleAddNote = async () => {
+    if (!validateForm()) {
+      return;
+    }
     try {
       setLoading(true);
       setError("");
       const data = await addNote(title, content, category_id);
 
-      setLoading(false);
+      setCategory_id("");
+      setTitle("");
+      setContent("");
+      navigate("/notes");
     } catch (error) {
       console.error(error);
-      setError(
-        error.response?.data?.message || "Login failed. Please try again",
-      );
+      console.log("ERRORS", error.response?.data);
+
+      const serverErrors = error.response?.data?.errors;
+
+      if (serverErrors) {
+        setErrors("");
+        const newErrors = {
+          title: "",
+          content: "",
+          category_id: "",
+        };
+
+        serverErrors.forEach((err) => {
+          newErrors[err.path] = err.msg;
+        });
+
+        setErrors(newErrors);
+      } else {
+        setError(error.response?.data?.message || "Failed to create note");
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -60,14 +141,16 @@ function AddNote() {
     <div>
       <h4>ADD NOTE</h4>
       <div style={{ margin: 10 }}>
-        {error && <p style={{ color: "red" }}>{error}</p>}
         <div style={{ marginBottom: 20 }}>
           <TextField
             onChange={handleChangeTitle}
             size="small"
             id=""
+            value={title}
+            required
+            error={Boolean(errors.title)}
+            helperText={errors.title}
             label="Title"
-            // defaultValue="Hello World"
           />
         </div>
         <div>
@@ -75,13 +158,16 @@ function AddNote() {
             onChange={handleChangeContent}
             size="small"
             id=""
+            value={content}
+            required
+            error={Boolean(errors.content)}
+            helperText={errors.content}
             label="Content"
-            // defaultValue="Hello World"
           />
         </div>
         <div>
           <Box sx={{ maxWidth: 220, marginTop: 2, size: "small" }}>
-            <FormControl fullWidth>
+            <FormControl fullWidth error={Boolean(errors.category_id)}>
               <InputLabel size="small" id="demo-simple-select-label">
                 Select Category
               </InputLabel>
@@ -91,6 +177,7 @@ function AddNote() {
                 value={category_id}
                 label="Category"
                 onChange={handleChangeCategoryId}
+                required
               >
                 {categories.map((category) => (
                   <MenuItem key={category.id} value={category.id}>
@@ -98,10 +185,15 @@ function AddNote() {
                   </MenuItem>
                 ))}
               </Select>
+              {errors.category_id && (
+                <p style={{ color: "red", fontSize: 12, marginLeft: 14 }}>
+                  {errors.category_id}
+                </p>
+              )}
             </FormControl>
           </Box>
         </div>
-        <Button onClick={handleAddNote}>
+        <Button disabled={loading} variant="outlined" onClick={handleAddNote}>
           {loading ? <CircularProgress /> : "SUBMIT"}
         </Button>
       </div>
